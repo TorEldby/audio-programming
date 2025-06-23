bdf17b104ebde3034f93bca363cbc5c8be51282d
Tor Eldby <eldby.tor@gmail.com>
2025-06-23 10:56:28 +0100

    Initial commit, made very simple audio test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include <SFML/Audio.hpp>
+
 int main() {
+    sf::Music music("music.mp3");
+    music.play();
+
+    while(music.getStatus() == sf::Music::Status::Playing) {
+    }
     return 0;
 }
